2e9a58a62f544028adf54aba005379ecec4933e2
MokhtarBaWahal <mokhtarsalem@aucegypt.edu>
2023-07-12 16:56:55 +0200

    adding more files and modules

--- src/helperFunctions.rs
+++ src/helperFunctions.rs
@@ -1,4 +1,6 @@
-
+use sysinfo::{CpuExt,Pid,  ProcessExt,System, SystemExt, PidExt, ProcessStatus, UserExt};
+use nix::sys::signal::{kill, Signal};
+use nix::unistd::Pid as nixPID;
 
 pub fn print_process_tree(items:  &[Vec<String>], id: String,  depth: usize) {
     let padding = "  ".repeat(depth);
@@ -25,5 +27,20 @@ pub fn print_process_tree(items:  &[Vec<String>], id: String,  depth: usize) {
     for child in children {
         print_process_tree(items, child, depth + 4);
     }
+
+}
+
+
+pub fn kill_process_and_children(pid: u32) {
+    let  system = System::new();
+    //let pid = Pid::from_raw(pid);
+    for (_pid2, process) in system.processes() {
+        if process.parent() == Some(Pid::from_u32(pid)) {
+            kill_process_and_children(process.pid().as_u32());
+        }
+    }
+     let pid = nixPID::from_raw(pid as i32);
+    kill(pid, Signal::SIGTERM);
+
 
 }
